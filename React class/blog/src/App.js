8a940b257829645575398f98fc1b9d9165734a6c
minji0123 <sso02027@naver.com>
2021-12-24 23:50:42 +0900

    21-12-24

--- React class/blog/src/App.js
+++ React class/blog/src/App.js
@@ -6,13 +6,13 @@ import './App.css';
 function App() {
 
     
-    let [글제목, 글제목변경] = useState(['남자 코트 추천', ' 강남 우동 맛집', '파이썬 독학']);
+    let [글제목, 글제목변경] = useState( ['남자 코트 추천', ' 강남 우동 맛집', '파이썬 독학'] );
     let [좋아요, 좋아요변경] = useState([0,0,0]);
-    let [좋아요1, 좋아요변경1] = useState(0);
     let [modal, modal변경] = useState(false); 
 
 
-    // 변수
+    let [입력값, 입력값변경] = useState('');
+
     let [누른제목, 누른제목변경] = useState(0);
 
     function 좋아요함수(idx){
@@ -28,29 +28,12 @@ function App() {
                 </div>
             </div>
 
-            {/* <div className='list'>
-                <h3> {글제목[0]} <span onClick={()=>{ 좋아요함수(0) }}> 👍 </span> {좋아요[0]} </h3>
-                    <p>2월 17일 발행</p> 
-                    <hr/> 
-            </div>
-
-
-            <div className = 'list'> 
-                <h3> {글제목[1]} <span onClick={ ()=>{ 좋아요함수(1)}}> 👍 </span> {좋아요[1]}   </h3>
-                    <p>2월 17일 발행 </p> 
-                    <hr/> 
-            </div> 
-
-            <div className = 'list' > 
-                <h3> {글제목[2]}<span onClick={ ()=>{ 좋아요함수(2)}}> 👍 </span> {좋아요[2]}   </h3>
-                <p>2 월 17일 발행</p> 
-                <hr/>
-            </div> */}
-
+            {/* map으로 돌린 함수는 key={}를 안써주면 개발자도구에서 warning이 뜬다.
+                파라미터의 정수값을 안에 넣어주기 */}
             {
                 글제목.map(function(글, i){
                     return(
-                        <div className = 'list' > 
+                        <div className = 'list' key={i}> 
                         <h3  onClick={()=>{ 누른제목변경(i) }}> {글제목[i]}<span onClick={ ()=>{ 좋아요함수(i)}}> 👍 </span> {좋아요[i]}   </h3>
                         <p>2 월 17일 발행</p> 
                         <hr/>
@@ -58,13 +41,29 @@ function App() {
                     )
                 })
             }
+            
+            {/* 글발행 기능을 만들거시야 */}
+            <div className='publish'>
+                <input onChange={ (e)=>{입력값변경(e.target.value)} }/>
+                <button onClick={ ()=>{
+                    var arrayCopy = [...글제목];
+                    arrayCopy.unshift(입력값); // unshift = array 맨 앞에 자료 추가해주세여
+                    글제목변경( arrayCopy );
 
-            {/* 버튼을 누르면 각각 다른 모달창이 뜨게 할거임 */}
-            <button onClick={()=>{ 누른제목변경(0) }}>버튼1</button>
-            <button onClick={()=>{ 누른제목변경(1) }}>버튼2</button>
-            <button onClick={()=>{ 누른제목변경(2) }}>버튼3</button>
+                    var arrayCopy2 = [0, ...좋아요];
+                    좋아요변경( arrayCopy2 );
+
+                } }>저장</button>
+            </div>
+
+            {/* 1. 사용자가 입력한 글을 '입력값'state 로 저장하기 
+                1.5. 실전 : 서버로 먼저 보내서 영구저장함
+                2. 저장버튼을 누르면 그 변수를 '글제목'state 에 추가함*/}
+
+
 
             <button onClick={()=>{modal변경(!modal)}}>열고닫기</button>
+
             {
                 modal===true
                 // <Modal 작명 = {전송할state} ></Modal>
@@ -75,15 +74,7 @@ function App() {
         </div>
     );
 }
-// 제목을 누를 때 각각 다른 모달창이 뜨게
 
-// 모달창 만드는 법
-// 모달창 보이는/안보이는 상태정보를 state에 저장하고
-    // state가 0일때는 0번째 제목 출력, 1일때는 1번째 제목 출력
-
-// ui 만드는 법
-// ui와 관련된 중요 정보들을 state로 저장해놓고
-// state에 따라서 ui가 수정되게 만들면 됨
 function Modal(props){
     return(
         <div className="modal">
