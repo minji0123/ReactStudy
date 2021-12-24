ec87707506231e28c9c343df29b70a53b81eab2d
minji0123 <sso02027@naver.com>
2021-12-24 16:28:21 +0900

    21-12-24

--- React class/blog/src/App.js
+++ React class/blog/src/App.js
@@ -8,17 +8,18 @@ function App() {
     
     let [글제목, 글제목변경] = useState(['남자 코트 추천', ' 강남 우동 맛집', '파이썬 독학']);
     let [좋아요, 좋아요변경] = useState([0,0,0]);
-
     let [좋아요1, 좋아요변경1] = useState(0);
-
     let [modal, modal변경] = useState(false); 
+
+
+    // 변수
+    let [누른제목, 누른제목변경] = useState(0);
 
     function 좋아요함수(idx){
         var newArray = [...좋아요];
         newArray[idx] = 좋아요[idx]+1;
         좋아요변경(newArray)
     }
-
 
     return (
         <div className="App">
@@ -27,7 +28,7 @@ function App() {
                 </div>
             </div>
 
-            <div className='list'>
+            {/* <div className='list'>
                 <h3> {글제목[0]} <span onClick={()=>{ 좋아요함수(0) }}> 👍 </span> {좋아요[0]} </h3>
                     <p>2월 17일 발행</p> 
                     <hr/> 
@@ -44,41 +45,49 @@ function App() {
                 <h3> {글제목[2]}<span onClick={ ()=>{ 좋아요함수(2)}}> 👍 </span> {좋아요[2]}   </h3>
                 <p>2 월 17일 발행</p> 
                 <hr/>
-            </div>
+            </div> */}
+
+            {
+                글제목.map(function(글, i){
+                    return(
+                        <div className = 'list' > 
+                        <h3  onClick={()=>{ 누른제목변경(i) }}> {글제목[i]}<span onClick={ ()=>{ 좋아요함수(i)}}> 👍 </span> {좋아요[i]}   </h3>
+                        <p>2 월 17일 발행</p> 
+                        <hr/>
+                    </div>
+                    )
+                })
+            }
+
+            {/* 버튼을 누르면 각각 다른 모달창이 뜨게 할거임 */}
+            <button onClick={()=>{ 누른제목변경(0) }}>버튼1</button>
+            <button onClick={()=>{ 누른제목변경(1) }}>버튼2</button>
+            <button onClick={()=>{ 누른제목변경(2) }}>버튼3</button>
 
             <button onClick={()=>{modal변경(!modal)}}>열고닫기</button>
             {
                 modal===true
                 // <Modal 작명 = {전송할state} ></Modal>
-                ? <Modal 글제목 = {글제목} ></Modal>
+                ? <Modal 글제목 = {글제목} 누른제목={누른제목}></Modal>
                 : null
             }
-
-
-            {/* {
-                // 반복될 때 마다 제목부분에 각각 다른 데이터가 들어가고 싶음
-                글제목.map(function(글,i){ // 글제목만큼 본문을 반복시켜야함
-                    return (
-                    <div className = 'list' > 
-                    <h3> {글}<span onClick={()=>{ 좋아요함수(i)}}> 👍 </span> {좋아요[i]}   </h3>
-                    <p>2 월 17 일 발행</p> 
-                    <hr/>
-                    </div>
-                    )
-                    
-                })
-            } */}
 
         </div>
     );
 }
-// App(){}안에 있는 글제목 state를 Modal(){}안에서 쓰고 싶으면??
-// App은 부모고, Modal은 자식이다
+// 제목을 누를 때 각각 다른 모달창이 뜨게
+
+// 모달창 만드는 법
+// 모달창 보이는/안보이는 상태정보를 state에 저장하고
+    // state가 0일때는 0번째 제목 출력, 1일때는 1번째 제목 출력
+
+// ui 만드는 법
+// ui와 관련된 중요 정보들을 state로 저장해놓고
+// state에 따라서 ui가 수정되게 만들면 됨
 function Modal(props){
     return(
         <div className="modal">
-            {/* 부모인 App 컴포넌트에 있는 글제목을 끌어다 써야 하기 때문에 props 함수를 사용해야 한다.*/}
-            <h2>{props.글제목[0]}</h2>
+            <h2>{props.글제목[props.누른제목]}</h2>
             <p>날씨</p>
             <p>상세내용 </p>
         </div>
