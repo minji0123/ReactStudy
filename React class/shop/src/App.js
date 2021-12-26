3ca8bbe76e2da7456201efe437650bad015ab762
minji0123 <sso02027@naver.com>
2021-12-26 16:05:37 +0900

    21-12-26

--- React class/shop/src/App.js
+++ React class/shop/src/App.js
@@ -1,12 +1,19 @@
+/* eslint-disable */
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import { Navbar,Button,Nav,NavDropdown,Form,FormControl,Jumbotron } from 'react-bootstrap';
+import Data from './data.js';
+import {Link, Route, Switch} from 'react-router-dom';
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
@@ -27,19 +34,75 @@ function App() {
   </Navbar.Collapse>
 </Navbar>
 
-<Jumbotron>
-  <h1>Hello, world!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-    <Button variant="primary">Learn more</Button>
-  </p>
-</Jumbotron>
+
+
+{/* <Route exact path="/">
+  <div>메인페이지에요</div>
+</Route>
+<Route path="/detail">
+  <div>디테일페이지에요</div>
+</Route> */}
+
+{/* <Route path="어쩌구" component={Modal}></Route> */}
+
+<Route exact path="/">
+    <Jumbotron className="background">
+      <h1>20% Season Off</h1>
+      <p>
+        This is a simple hero unit, a simple jumbotron-style component for calling
+        extra attention to featured content or information.
+      </p>
+      <p>
+        <Button variant="primary">Learn more</Button>
+      </p>
+    </Jumbotron>
+
+    <div className="container">
+      <div className="row">
+      {
+        shoes.map((a, i)=>{
+          // return <Card shoes={shoes[i]}/>
+          return <Card shoes={a} i={i} key={i}/>
+        })
+      }
+      </div>
+
+    </div>
+    
+
+</Route>
+<Route path="/detail">
+  <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+</div> 
+</Route>
+
 
     </div>
   );
 }
 
+function Card(props){
+  return(
+        <div className='col-md-4'>
+          <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i + 1) +'.jpg' } width="100%"/>
+          <h4>{props.shoes.title}</h4>
+          <p> {props.shoes.content} & {props.shoes.price}</p>
+        </div>
+  )
+}
+
+
+// src에 넣은 파일은 파일명이 변경되고 압축됨
+// 근데 public 에 넣으면 보존됨 근데 절대경로로 사용해야됨
 export default App;
